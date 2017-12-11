48fd4568f063243e7738aa909e4399138ec83089
John Piasecki <jgpie189@outlook.com>
2017-12-11 14:26:34 -0500

    added the sout lines and processor class

---
+++ net.fccc.order_processing.13909/src/net/fccc/order_processing/pkg13909/NetFcccOrder_processing13909.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.fccc.order_processing.pkg13909;
+
+import java.io.*;
+
+
+
+/**
+ *
+ * @author jpiasecki
+ */
+public class NetFcccOrder_processing13909 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException{
+        
+        System.out.println("The list of orders is being processed please wait.");
+        
+        OrderProcessor.ReadOrders();
+        
+        System.out.println("The list of oreders has properly been processed and "
+                + "printed out to the 'OrdesProcessed.txt file' ");
+    }
+    
+   
+    
+    
+}
