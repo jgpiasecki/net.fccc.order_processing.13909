71292db9372fcf1109382ed63dc91673d64c6453
John Piasecki <jgpie189@outlook.com>
2017-12-14 13:55:39 -0500

    Finished the project and commented on the project

--- net.fccc.order_processing.13909/src/net/fccc/order_processing/pkg13909/OrderProcessor.java
+++ net.fccc.order_processing.13909/src/net/fccc/order_processing/pkg13909/OrderProcessor.java
@@ -19,27 +19,28 @@ import java.io.PrintWriter;
 public class OrderProcessor {
         public static void ReadWrite() throws IOException 
     {
-        
-        
-        
-
+        //trys to create a buffered reader to read the Oders file
         try(BufferedReader br = new BufferedReader (new FileReader("Orders.txt")))
         {   
+            //calls the reader to rea the jirst line
            br.readLine();
             
+           //reads the second line in orders and sets it equal to a string named line
             String line = br.readLine();
             
-            
+            /*trys to create a buffered writer named write order to create a file
+             * if the file has not been created yet. if it has it will 
+             * write to the file
+             */
             try(PrintWriter writeOrders = new PrintWriter(new BufferedWriter(new FileWriter("OrdersProcessed.txt",true))))
             {
+                //as long as line is not empty it will run this loop
                 while( line !=null )
                 {
-                
-                
-                
+                    //Creates a string array named items and splits line up at the |
                     String[] items = line.split("\\|");
                 
-                
+                    //assigns the proper variables to their values
                     String orderID = items[0];
                     String partNum = items[1];
                     double price = Double.valueOf(items[2]);
@@ -48,28 +49,34 @@ public class OrderProcessor {
                     double tax = solveTax();
                     double shipping = solveShipping();
                     
+                    //prints out the proper variables and their values to the file
                     writeOrders.println("Order ID: " + orderID);     
                     writeOrders.println("Part Number: " + partNum);
                     writeOrders.println("Price: " + price);
                     writeOrders.println("Quantity: " + quantity);
-                    writeOrders.println("Tax: " + subtotal + tax);
-                    writeOrders.println("Shipping: " + subtotal + shipping);
+                    writeOrders.println("Tax: " + subtotal * tax);
+                    writeOrders.println("Shipping: " + subtotal * shipping);
                     writeOrders.println("Toatal: " + subtotal + tax + shipping);
-                    br.readLine();
+                    
+                    //tells the reader to read the next line
+                    line = br.readLine();
                 }
             
-            
+            //closes the writer
             writeOrders.close(); 
             }
             
+            //if an error is caught while writing then this error exception will be thrown
             catch(IOException e)
             {
-                System.out.println("Error: " +e.getLocalizedMessage());
+                System.out.println("Error: " +e.getMessage());
             }
             
-            
+            //closes the reader
+            br.close();
         }
        
+        // if an error is caught while reading the file then this error exception is thown
         catch(IOException e)
         {
             System.out.println("Error: " + e.getMessage());
@@ -79,6 +86,7 @@ public class OrderProcessor {
         
     }
     
+    //a method that sets a final taxrate
     public static double solveTax()
     {
         final double TAX_RATE = 0.02;
@@ -86,11 +94,12 @@ public class OrderProcessor {
         return TAX_RATE;
     }
     
+    //a method that sets a final shipping rate
     public static double solveShipping()
     {
-        final double SHIPPING = 0.05;
+        final double SHIPPING_RATE = 0.05;
         
-        return SHIPPING;
+        return SHIPPING_RATE;
     }
     
 }
